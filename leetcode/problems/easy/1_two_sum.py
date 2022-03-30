747a7d5fd3228d09374e9399b496a77c09970ddd
Artem Ryblov <ryblovartem@gmail.com>
2022-03-30 19:11:36 +0300

    freshened in mind two sum problem and solutions

--- leetcode/problems/easy/1_two_sum.py
+++ leetcode/problems/easy/1_two_sum.py
@@ -3,36 +3,27 @@ from typing import List
 
 class Solution:
     def twoSum_1(self, nums: List[int], target: int) -> List[int]:
-        '''
+        """
         Brute Force
-        :param nums:
-        :param target:
-        :return:
-        '''
+        """
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if nums[i] + nums[j] == target and i != j:
                     return [i, j]
 
     def twoSum_2(self, nums: List[int], target: int) -> List[int]:
-        '''
+        """
         Brute Force fixed a bit
-        :param nums:
-        :param target:
-        :return:
-        '''
+        """
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] == target - nums[j]:
                     return [i, j]
 
     def twoSum_3(self, nums: List[int], target: int) -> List[int]:
-        '''
+        """
         Two-pass Hash Table
-        :param nums:
-        :param target:
-        :return:
-        '''
+        """
         hashmap = {}
         for i in range(len(nums)):
             hashmap[nums[i]] = i
@@ -42,12 +33,9 @@ class Solution:
                 return [i, hashmap[complement]]
 
     def twoSum_4(self, nums: List[int], target: int) -> List[int]:
-        '''
+        """
         One-pass Hash Table
-        :param nums:
-        :param target:
-        :return:
-        '''
+        """
         hashmap = {}
         for i in range(len(nums)):
             complement = target - nums[i]
@@ -60,8 +48,12 @@ def main():
     solution = Solution()
     nums = [2, 7, 11, 15]
     target = 9
-    res = solution.twoSum_4(nums=nums, target=target)
+    res = solution.twoSum_3(nums=nums, target=target)
     print(res)
+
+    assert set(solution.twoSum_3(nums=[2, 7, 11, 15], target=9)) == {0, 1}
+    assert set(solution.twoSum_3(nums=[3, 2, 4], target=6)) == {1, 2}
+    assert set(solution.twoSum_3(nums=[3, 3], target=6)) == {0, 1}
 
 
 if __name__ == '__main__':
